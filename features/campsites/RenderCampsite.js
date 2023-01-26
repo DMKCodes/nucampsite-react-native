427ab8af4bd120692574f6750f7174bead568eeb
Code <dmkcodes@gmail.com>
2023-01-26 14:07:06 -0500

    add res screen alert and add'l. gestures

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -10,14 +10,15 @@ const RenderCampsite = (props) => {
     const view = useRef();
 
     const isLeftSwipe = ({ dx }) => dx < -200;
+    const isRightSwipe = ({ dx }) => dx > 200;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
             view.current
                 .rubberBand(1000)
-                .then((endState) => 
-                console.log(endState.finished ? 'finished' : 'canceled')
+                .then((endState) =>
+                    console.log(endState.finished ? 'finished' : 'canceled')
                 );
         },
         onPanResponderEnd: (e, gestureState) => {
@@ -41,6 +42,8 @@ const RenderCampsite = (props) => {
                     ],
                     { cancelable: false }
                 );
+            } else if (isRightSwipe(gestureState)) {
+                props.onShowModal();
             }
         }
     });
